bccb40c364040de90f49eda63ea5c8243e400fd3
Tim Garrity <tag.garrity@gmail.com>
2019-01-28 20:03:05 -0700

    set up connect

---
+++ src/components/LibraryList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class LibraryList extends Component {
+    constructor(props) {
+        super(props);
+    }
+    
+    render() {
+        return;
+    }
+}
+
+export default LibraryList;
